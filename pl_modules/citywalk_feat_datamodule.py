0cf9b9580cebe1b9216ad3314de9f6dfb9db5e62
Harsh Sutaria <h.sutaria@nyu.edu>
2025-10-23 00:36:52 -0400

    custom dataloader

--- pl_modules/citywalk_feat_datamodule.py
+++ pl_modules/citywalk_feat_datamodule.py
@@ -1,10 +1,55 @@
 # data/datamodule.py
 
 import pytorch_lightning as pl
+import torch
 from torch.utils.data import DataLoader
 from data.citywalk_dataset import CityWalkSampler
 from data.citywalk_feat_dataset import CityWalkFeatDataset
 from torch.utils.data.distributed import DistributedSampler
+
+
+def citywalk_collate(batch):
+    """Custom collate function that avoids resizing non-resizable storages."""
+
+    def _collate_recursive(items):
+        template = next((item for item in items if item is not None), None)
+        if template is None:
+            return None
+
+        if isinstance(template, torch.Tensor):
+            filled = []
+            for item in items:
+                if item is None:
+                    filled.append(torch.zeros_like(template))
+                else:
+                    filled.append(item.clone())
+            return torch.stack(filled, dim=0)
+
+        if isinstance(template, (float, int)):
+            dtype = torch.float32 if isinstance(template, float) else torch.long
+            values = [0 if item is None else item for item in items]
+            return torch.tensor(values, dtype=dtype)
+
+        if isinstance(template, dict):
+            keys = set()
+            for item in items:
+                if isinstance(item, dict):
+                    keys.update(item.keys())
+            return {
+                key: _collate_recursive(
+                    [
+                        item.get(key) if isinstance(item, dict) else None
+                        for item in items
+                    ]
+                )
+                for key in keys
+            }
+
+        # Fallback: keep raw objects (e.g., strings) as lists
+        return items
+
+    return _collate_recursive(batch)
+
 
 class CityWalkFeatDataModule(pl.LightningDataModule):
     def __init__(self, cfg):
@@ -27,14 +72,29 @@ class CityWalkFeatDataModule(pl.LightningDataModule):
             sampler = DistributedSampler(self.train_dataset, shuffle=True)
         else:
             sampler = CityWalkSampler(self.train_dataset)
-        
-        return DataLoader(self.train_dataset, batch_size=self.batch_size,
-                          num_workers=self.num_workers, sampler=sampler)
+
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            sampler=sampler,
+            collate_fn=citywalk_collate,
+        )
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size,
-                          num_workers=self.num_workers, shuffle=False)
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+            collate_fn=citywalk_collate,
+        )
 
     def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size,
-                          num_workers=self.num_workers, shuffle=False)
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
+            collate_fn=citywalk_collate,
+        )
